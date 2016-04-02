5b7eca65c39fbead6376c321465aa41b467a339e
tolkienfan2 <minni.ang@gmail.com>
2016-04-02 07:34:51 -0400

    Music and search function added

---
+++ PokedexByCreativeIce/PokemonDetailVC.swift
@@ -0,0 +1,22 @@
+//
+//  PokemonDetailVC.swift
+//  PokedexByCreativeIce
+//
+//  Created by Minni K Ang on 2016-04-01.
+//  Copyright © 2016 Minni K Ang. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    
+    var pokemon: Pokemon!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
